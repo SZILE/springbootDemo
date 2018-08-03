9eadf82e2501b9b425d0670b1514369e99b71c46
SZILE <413387105@qq.com>
2018-08-03 18:50:04 +0800

    001

---
+++ springboot-test-core/src/main/java/org/springboot/test/core/config/BaseConfig.java
@@ -0,0 +1,15 @@
+package org.springboot.test.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * @author ShiPC
+ *
+ * 2018年8月3日
+ */
+@Configuration
+@PropertySource(value="classpath:appliction-${spring.profile.active}.properties",encoding="UTF-8")
+public class BaseConfig {
+
+}
